1f16a55903282e93cf6a7b9008cfe931cc2ad89e
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-08-05 16:00:11 +0530

    Implementing new callback with error parameter

---
+++ app/src/main/java/zolostays/zolo/Utils/OnProcessFinishedErrorMsgCallback.java
@@ -0,0 +1,10 @@
+package zolostays.zolo.Utils;
+
+/**
+ * Created by mallikapriyakhullar on 05/08/17.
+ */
+
+public interface OnProcessFinishedErrorMsgCallback {
+    public void onError(String msg);
+    public void onSuccess();
+}
